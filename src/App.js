8e43fcbce5f7a043f9544ada1824b68f8dfb2bec
David Sotto Mayor <ddsmmf@gmail.com>
2021-08-23 22:27:13 -0400

    Initializing with React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Layout from './Layout/Layout';
+import Button from 'react-bootstrap/Button';
+
 
 const App = () => {
   return (
@@ -7,6 +9,7 @@ const App = () => {
     <Layout>
       <div>
         <h2>Does this work?</h2>
+        <Button variant="success">Primary</Button>
       </div>
     </Layout>
   )
