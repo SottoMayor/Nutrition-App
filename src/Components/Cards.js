bf71206e95cb99d6d2368b9d7326afa209d9938d
David Sotto Mayor <ddsmmf@gmail.com>
2021-08-24 22:03:31 -0400

    Handling errors in when we get the data from the API

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,25 +1,32 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import Card from './Card';
-import Spinner from 'react-bootstrap/Spinner'
+import Spinner from 'react-bootstrap/Spinner';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert'
 
 const Cards = () => {
 
-    const [ infos, setInfos ] = useState([]);
-    const [loading, setLoading] = useState(false)
+    const [infos, setInfos] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [message, setMessage] = useState('');
 
     const fetchData = useCallback( async () => {
+        setError(false);
         setLoading(true);
         try{
             const response = await fetch('https://sujeitoprogramador.com/rn-api/?api=posts/');
+            if(!response.ok){
+                throw new Error('Algo deu errado! Tente novamente.')
+            }
+
             const data = await response.json();
 
-            if(!response.ok){
-                throw new Error('Something went wrong!')
-            }
             setInfos(data);
         }catch(err){
-            // make the error handling
-            console.log(err.message);
+            setMessage(err.message);
+            setError(true);
         }
 
         setLoading(false);
@@ -35,8 +42,32 @@ const Cards = () => {
 
             {loading && <Spinner animation="border" role="status"/>}
 
+            {error && (
+                <Modal show={error} onHide={() => setError(false)}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Ocorreu um ERRO!</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body><strong>Causa</strong>: {message}</Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="danger" onClick={() => setError(false)}>
+                        Entendido
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+            )}
+
+            {!loading && infos.length === 0 && (
+                <Alert variant='warning'>
+                    <Alert.Heading>Opa! Você não possui postagens cadastradas.</Alert.Heading>
+                    <p>
+                    Isso aconteceu porque você realmete não possui postagens castradas, ou tivemos problemas para
+                    carregar suas postagens!
+                    </p>
+              </Alert>
+            )}
+
             {
-                infos.map( info => <Card {...info} key={info.id}/> )
+                !error && infos.map( info => <Card {...info} key={info.id}/> )
             }
         </section>
     )
