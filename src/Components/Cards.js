44d4e9704ae8a3fac23b17d463df257a4b9091a7
David Sotto Mayor <ddsmmf@gmail.com>
2021-08-24 21:36:09 -0400

    Displaying a Spinner when fetching data

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import Card from './Card';
+import Spinner from 'react-bootstrap/Spinner'
 
 const Cards = () => {
 
     const [ infos, setInfos ] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     const fetchData = useCallback( async () => {
-        const response = await fetch('https://sujeitoprogramador.com/rn-api/?api=posts/');
-        const data = await response.json();
-        setInfos(data);
+        setLoading(true);
+        try{
+            const response = await fetch('https://sujeitoprogramador.com/rn-api/?api=posts/');
+            const data = await response.json();
+
+            if(!response.ok){
+                throw new Error('Something went wrong!')
+            }
+            setInfos(data);
+        }catch(err){
+            // make the error handling
+            console.log(err.message);
+        }
+
+        setLoading(false);
+        
     }, [])
 
     useEffect(() => {
@@ -17,6 +32,9 @@ const Cards = () => {
 
     return (
         <section style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+
+            {loading && <Spinner animation="border" role="status"/>}
+
             {
                 infos.map( info => <Card {...info} key={info.id}/> )
             }
